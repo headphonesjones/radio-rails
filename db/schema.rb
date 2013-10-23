0d8dc7be141f4fb71a7d2189173dde34c243ebc5
Jordan Johnson <josamjohn@gmail.com>
2013-10-23 00:07:15 -0500

    Remove events extra column, add dates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121023102705) do
+ActiveRecord::Schema.define(:version => 20131023045543) do
 
   create_table "applications", :force => true do |t|
     t.string   "first_name"
@@ -94,12 +94,10 @@ ActiveRecord::Schema.define(:version => 20121023102705) do
 
   create_table "events", :force => true do |t|
     t.string   "title"
-    t.string   "location"
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
-    t.string   "first_line"
-    t.string   "second_line"
+    t.date     "date"
   end
 
   create_table "hostings", :force => true do |t|
